43510f042ec9bd4ff496c1d10d5c6d4a7b1e82ad
Tomas Andersson <tomas.j.andersson@gmail.com>
2020-10-20 21:12:07 +0200

    Add test of connection after setup

--- ArasDevToolCommonCommands/Command/Commands/SetupCommand.cs
+++ ArasDevToolCommonCommands/Command/Commands/SetupCommand.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Hille.Aras.DevTool.Interfaces.Command;
 using Hille.Aras.DevTool.Interfaces.Configuration;
 
-namespace ArasDevTool.Command.Commands {
+namespace Hille.Aras.DevTool.Common.Commands.Command.Commands {
     public class SetupCommand : ICommand {
         public string Name => "Setup";
 
@@ -17,6 +17,22 @@ namespace ArasDevTool.Command.Commands {
         public void Run() {
             ArasXmlStoredConfig config = new ArasXmlStoredConfig();
             config.Setup();
+            // Test Connection
+            TestConnection();
+         
+        }
+
+        private void TestConnection() {
+            var testCommand = new ArasCommands.TestArasConnectionCommand
+            {
+                Logger = new Hille.Aras.DevTool.Interfaces.Logging.BasicLogger()
+            };
+            var inputArgs = new List<string>
+            {
+                testCommand.Name
+            };
+            if (testCommand.ValidateInput(inputArgs))
+                testCommand.Run();
         }
 
         public bool ValidateInput(List<string> inputArgs) {
