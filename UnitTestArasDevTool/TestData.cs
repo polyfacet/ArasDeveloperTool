511c6a4920557de63460ccbb956dfa21697db174
Tomas Andersson <tomas.j.andersson@gmail.com>
2020-10-23 13:36:37 +0200

    Refactor tests

---
+++ UnitTestArasDevTool/TestData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestArasDevTool {
+    class TestData {
+
+        public const string URL = "http://localhost/HCAras12";
+        public const string DB = "HCAras12";
+        public const string USER = "admin";
+        public const string PASSWORD = "innovator";
+
+    }
+}
