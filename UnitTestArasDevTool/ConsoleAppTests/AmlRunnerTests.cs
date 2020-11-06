c963a1c8934a4385b740c7d51dc41e2164fbc096
Tomas Andersson <tomas.j.andersson@gmail.com>
2020-11-06 16:12:06 +0100

    Basic test setup for AmlRunner

---
+++ UnitTestArasDevTool/ConsoleAppTests/AmlRunnerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ArasDevTool;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestArasDevTool.ConsoleAppTests {
+
+    [TestClass]
+    [TestCategory("ConsoleTests")]
+    public class AmlRunnerTests : ConsoleAppTestBase {
+
+
+        public override string Command => "AmlRunner";
+
+        [TestMethod]
+        public void AmlRunnerStoredConnection() {
+            AssertRun(@"-mf ""TestData\AMLRunner\AMLRunnerTest.mf""", (int)Program.Result.OK);
+        }
+
+        [TestMethod]
+        public void AmlRunnerConnectionString() {
+            AssertRun(CS_CONNENCTION_STRING + " -mf \"TestData\\AMLRunner\\AMLRunnerTest.mf\"", (int)Program.Result.OK);
+        }
+
+        [TestMethod]
+        public void CheckLatestUpdatesHelp() {
+            AssertRun(HELP_FLAG, (int)Program.Result.HELP);
+        }
+
+    }
+}
