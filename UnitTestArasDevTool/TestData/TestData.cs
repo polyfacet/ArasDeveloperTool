8abe15431d69137de8bd9cc2e8cb6de8141f3c56
Tomas Andersson <tomas.j.andersson@gmail.com>
2023-05-19 14:18:43 +0200

    test: Updates default config tests

--- UnitTestArasDevTool/TestData/TestData.cs
+++ UnitTestArasDevTool/TestData/TestData.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace UnitTestArasDevTool {
     class TestData {
 
-        public const string URL = "http://localhost/dev";
-        public const string DB = "DevR12SP9";
+        public const string URL = "http://localhost/2023";
+        public const string DB = "2023";
         public const string USER = "admin";
         public const string PASSWORD = "innovator";
 
